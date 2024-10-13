551481cb21427d535ed4c4c2d6d35970b9d057d7
sollall <komugikoakasya@gmail.com>
2024-10-13 19:23:08 +0900

    mid layer for debug.py

--- scripts/sample.py
+++ scripts/sample.py
@@ -2,22 +2,27 @@ import time
 
 start = time.time()
 
-try:
-    from pathlib import Path
-    import sys
 
-    sys.path.append(str(Path(__file__).resolve().parent.parent))
-    from utils.inout import replace_io
+def main(prob_id=None):
+    try:
+        from pathlib import Path
+        import sys
 
-    PROB_NO = sys.argv[1]
-    debug_mode = True
-    replace_io(PROB_NO)
-except ModuleNotFoundError:
-    debug_mode = False
+        sys.path.append(str(Path(__file__).resolve().parent.parent))
+        from utils.inout import replace_io
+
+        debug_mode = True
+        if prob_id is None:
+            raise ValueError("Please specify the problem in debug mode.")
+        replace_io(prob_id)
+    except ModuleNotFoundError:
+        debug_mode = False
+    
+    solve()
 
 def solve():
     for _ in range(2):
         print(input())
 
 if __name__ == "__main__":
-    solve()
+    main(0)

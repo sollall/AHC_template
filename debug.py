7412181d06e0ffceb64bfc7707ecfa8bfe2d06e4
sollall <komugikoakasya@gmail.com>
2024-10-16 14:22:19 +0900

    run sample,main using conf

--- debug.py
+++ debug.py
@@ -6,33 +6,40 @@ import logging
 import optuna
 import hydra
 import mlflow
+from omegaconf import DictConfig
 
 logging.basicConfig(level=logging.INFO)
 
-def run_module(module,prob_no):
-    return module.main(prob_no)
-
-@hydra.main(config_path='./conf', config_name='config', version_base="1.3")
-def main(cfg):
-
-    module = importlib.import_module(cfg.general.module_name)
+def run_module(module_name:str,params_opt:DictConfig,prob_no):
+    module = importlib.import_module(module_name)
     if hasattr(module, "main"):
         pass
     else:
-        raise Exception(f"Module '{cfg.general.module_name}' does not have a 'main' function.")
+        raise Exception(f"Module '{module_name}' does not have a 'main' function.")
+
+    return module.main(params_opt,prob_id=prob_no)
+
+@hydra.main(config_path='./conf', config_name='config', version_base="1.3")
+def main(cfg:DictConfig):
 
     if cfg.general.executor_type=="ProcessPoolExecutor":
         executor_class=ProcessPoolExecutor
     else:
         raise ValueError("Please specify the executor type.")
-    with executor_class(max_workers=cfg.general.max_workers) as executor:
-        results = executor.map(run_module,
-                                [module for _ in range(cfg.general.num_tests)],
-                                [i for i in range(cfg.general.num_tests)]
-                            )
 
     total_score,total_time=0,0
-    
+    with executor_class(max_workers=cfg.general.max_workers) as executor:
+        results = executor.map(run_module,
+                                [cfg.general.module_name for _ in range(cfg.general.num_tests)],
+                                [cfg.optimizer for _ in range(cfg.general.num_tests)],
+                                [i for i in range(cfg.general.num_tests)]
+                            )
+        for result in results:
+            score,passed_time=result
+            total_score+=score
+            total_time+=passed_time
+
+    logging.info(total_score)
     return total_score
 
 if __name__ == "__main__":
--- scripts/sample.py
+++ scripts/sample.py
@@ -1,6 +1,6 @@
 import time
 
-def main(prob_id=None):
+def main(params,prob_id=None):
     start = time.time()
     try:
         from pathlib import Path
@@ -15,15 +15,17 @@ def main(prob_id=None):
     except ModuleNotFoundError:
         debug_mode = False
     
-    score=solve()
+    score=solve(**params)
     passed_time=time.time()-start
 
     return score, passed_time
 
-def solve():
+### 以下が問題ごとに別途作成が必要な部分
+
+def solve(epsilon,cooling_rate,epoch):
     for _ in range(2):
         print(input())
-    return 0
+    return epoch
 
 if __name__ == "__main__":
-    main(0)
+    solve()
